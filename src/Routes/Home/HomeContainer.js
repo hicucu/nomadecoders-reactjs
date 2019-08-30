073c4c01b8697ae785a93e75532d66c55784e85b
hicucu <hicucu@gmail.com>
2019-08-30 18:31:22 +0900

    feat: Home Comtainer

---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,53 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+import { moviesApi } from "../../api";
+
+export default class extends React.Component {
+  state = {
+    nowPlaying: null,
+    upcoming: null,
+    popular: null,
+    error: null,
+    loading: true
+  };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying }
+      } = await moviesApi.nowPlaying();
+      const {
+        data: { results: upcoming }
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: popular }
+      } = await moviesApi.popular();
+
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
+    } catch {
+      this.setState({ error: "Can't find movies iinformation." });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
+  render() {
+    const { nowPlaying, upcoming, pupular, error, loading } = this.state;
+    console.log(this.state);
+    return (
+      <HomePresenter
+        nowPlaying={nowPlaying}
+        upcoming={upcoming}
+        populra={pupular}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
