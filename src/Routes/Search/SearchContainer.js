b6a25e709b906ea29948fbc57eb6813835fe8997
hicucu <hicucu@gmail.com>
2019-08-30 18:32:24 +0900

    feat:Search Container

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,59 @@
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "../../api";
+
+export default class extends React.Component {
+  state = {
+    movieResults: null,
+    tvResult: null,
+    searchTerm: "",
+    loading: null,
+    error: null
+  };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    try {
+      const {
+        data: { results: movieResults }
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResult }
+      } = await tvApi.search(searchTerm);
+
+      this.setState({
+        movieResults,
+        tvResult
+      });
+    } catch {
+      this.setState({
+        error: "Can't find results."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  };
+
+  render() {
+    const { movieResults, tvResult, searchTerm, loading, error } = this.state;
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResult={tvResult}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
+}
